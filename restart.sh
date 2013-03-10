857947f441eba4bf3de26b3a52d2feea9ae32ab2
Dirk Colbry <colbrydi@msu.edu>
2013-03-09 20:37:16 -0500

    Added big direction

--- restart.sh
+++ restart.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -login
-pit=`qsub -t 1 pitremove_small.qsub`
+pit=`qsub -t 1-8 pitremove_small.qsub`
 echo $pit
-qsub -W depend=afteranyarray:$pit d8flowdir_small.qsub
+qsub -t 1-2 -W depend=afteranyarray:$pit d8flowdir_small.qsub
+
+pit=`qsub -t 1-8 pitremove_big.qsub`
+echo $pit
+qsub -t 1-2 -W depend=afteranyarray:$pit d8flowdir_big.qsub
 
