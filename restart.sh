025e3e6cb1d96da9d333b0dc8cc4cb591ddbdf2b
Dirk Colbry <colbrydi@msu.edu>
2013-03-13 07:59:35 -0400

    updated scripts

--- restart.sh
+++ restart.sh
@@ -1,9 +1,18 @@
 #!/bin/bash -login
+
+rm STOP_FILE
+
+echo "qsub -t 1-8 pitremove_small.qsub"
 pit=`qsub -t 1-8 pitremove_small.qsub`
 echo $pit
-qsub -t 1-2 -W depend=afteranyarray:$pit d8flowdir_small.qsub
+pr=${pit%.cmgr01}
+echo "qsub -t 1-2 -W depend=afteranyarray:$pr d8flowdir_small.qsub"
+qsub -t 1-2 -W depend=afteranyarray:$pr d8flowdir_small.qsub
 
+echo "qsub -t 1-8 pitremove_big.qsub"
 pit=`qsub -t 1-8 pitremove_big.qsub`
 echo $pit
-qsub -t 1-2 -W depend=afteranyarray:$pit d8flowdir_big.qsub
+pr=${pit%.cmgr01}
+echo "qsub -t 1-2 -W depend=afteranyarray:$pr d8flowdir_big.qsub"
+qsub -t 1-2 -W depend=afteranyarray:$pr d8flowdir_big.qsub
 
